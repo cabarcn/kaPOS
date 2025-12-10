1eb028b0482a05ec77f2f34d00405c1e9b0ef714
Cristopher Abarca <cristopher.abarca.navarro@gmail.com>
2025-12-10 17:51:25 -0300

    Ajustar include de apps a backend.* en urls del proyecto

--- urls.py
+++ urls.py
@@ -5,10 +5,10 @@ from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/usuarios/', include('usuarios.urls')),
-    path('api/clientes/', include('clientes.urls')),
-    path('api/planes/', include('planes.urls')),
-    path('api/suscripciones/', include('suscripciones.urls')),
+    path('api/usuarios/', include('backend.usuarios.urls')),
+    path('api/clientes/', include('backend.clientes.urls')),
+    path('api/planes/', include('backend.planes.urls')),
+    path('api/suscripciones/', include('backend.suscripciones.urls')),
     path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
     path('api/docs/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
    
